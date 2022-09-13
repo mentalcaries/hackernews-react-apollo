59d378ebeda3ed4a850b83fcc74b3bcf64965da3
Devin Jaggernauth <neexis@gmail.com>
2022-09-13 06:22:16 -0400

    create login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import LinkList from '../LinkList';
 import CreateLink from '../CreateLink';
 import Header from '../Header';
 import { Routes, Route } from 'react-router-dom';
+import Login from '../Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
